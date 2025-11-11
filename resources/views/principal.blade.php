14c191643fbac790ea1a1c5c86f72fe6931ca7fa
SamuelPeAg <spa0004@alu.medac.es>
2025-11-11 20:44:58 +0100

    principal

---
+++ resources/views/principal.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>pricipal</title>
+</head>
+<body>
+    <h1>factomove</h1>
+    <p>Samuel Peña</p>
+    <p>Jesus Panadero</p>
+    <p>Carlos Hurtado</p>
+    <p>Enrique Paez</p>
+    <p>Josemi Ramirez</p>
+</body>
+</html>
