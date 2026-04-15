1a45c7a56599f58b06d2c1a931e849b32842fcaa
SamuelPeAg <spa0004@alu.medac.es>
2026-04-15 11:50:36 +0200

    correcion de cosas en produccion

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -11,6 +11,7 @@ window.axios = axios;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 window.axios.defaults.baseURL = window.AppConfig?.baseUrl || '/';
 window.axios.defaults.withCredentials = true;
+window.axios.defaults.withXSRFToken = true;
 
 let token = document.head.querySelector('meta[name="csrf-token"]');
 if (token) {
